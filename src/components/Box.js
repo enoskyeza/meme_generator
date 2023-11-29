184b70b7037eecc3229d2bdc57856fbe3aceba72
enoskyeza <enoskyeza@gmail.com>
2023-11-29 22:07:03 +0300

    Start box challenges

---
+++ src/components/Box.js
@@ -0,0 +1,19 @@
+/**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+*/
+
+import React from "react"
+import boxes from "./boxes"
+
+export default function App() {
+    return (
+        <main>
+            <h1>Boxes will go here</h1>
+        </main>
+    )
+}
