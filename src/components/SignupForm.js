9739be16a0672b1e89a7437ed4a480a0f18c2636
enoskyeza <enoskyeza@gmail.com>
2023-12-29 03:26:50 +0300

    Add form styles

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -3,26 +3,31 @@ import "@/components/SignupForm.css"
 
 export default function SignupForm(){
     return(
-        <div className="form-bg">
-            <input
-                type="text"
-                placeholder="Email address"
-            />
-            <input
-                type="text"
-                placeholder="Password"
-            />
-            <input
-                type="text"
-                placeholder="Confirm Password"
-            />
-            <input
-                type="checkbox"
-            />
-            <label htmlFor="subscirbe">I want to join the newsletter</label>
-
-            <br/>
-            <button>Sign Up</button>
+        <div >
+            <form className="form-bg">
+                <div className="input-section">
+                    <input
+                        type="text"
+                        placeholder="Email address"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Password"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Confirm Password"
+                    />
+                </div>
+                <div className="checkbox-input">
+                    <input
+                        className=""
+                        type="checkbox"
+                    />
+                    <label htmlFor="subscirbe">I want to join the newsletter</label>
+                </div>
+                <button>Sign Up</button>
+            </form>
         </div>
     )
 }
