1f9697fb178c031de676b1cd392dea568ba2a7c5
enoskyeza <enoskyeza@gmail.com>
2024-01-07 18:34:37 +0300

    update data in DOM

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -10,7 +10,7 @@ function UseEffectApp() {
     React.useEffect(function() {
         fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setStarWarsData(data))
     }, [0])
 
     return (
