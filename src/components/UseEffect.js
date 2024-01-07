deb1a9dd337beb0510e6837680dc4e644099a21c
enoskyeza <enoskyeza@gmail.com>
2024-01-07 17:36:06 +0300

    test use effect example

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,1 +1,21 @@
 import React from React
+
+export default function useEffectApp() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+
+    console.log("Component rendered")
+
+    // side effects
+    React.useEffect(function() {
+        console.log("Effect ran")
+    }, [0])
+
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+        </div>
+    )
+}
