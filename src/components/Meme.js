3a08fa69721c71f218b85f0b4acc5096b01f8798
enoskyeza <enoskyeza@gmail.com>
2024-01-08 12:35:47 +0300

    fetch meme data from api

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,6 +6,23 @@ import react from "react"
 
 export default function Meme() {
 
+
+/**
+     * Challenge:
+     * As soon as the Meme component loads the first time,
+     * make an API call to "https://api.imgflip.com/get_memes".
+     *
+     * When the data comes in, save just the memes array part
+     * of that data to the `allMemes` state
+     *
+     * Think about if there are any dependencies that, if they
+     * changed, you'd want to cause to re-run this function.
+     *
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` blocks to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
+*/
+
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
@@ -33,10 +50,17 @@ export default function Meme() {
         }))
     }
 
+    const [allMemes, setAllMemes] = React.useState({})
 
+    fetch(`https://api.imgflip.com/get_memes`)
+    .then(res => res.json())
+    .then(data => setAllMemes(data))
 
     return (
         <main>
+            <div className="meme-data">
+                <pre>{JSON.stringify(allMemes, null, 2)}</pre>
+            </div>
             <div className="form">
                 <input
                     type="text"
