cbabe3b2b65e825a32323dcded72b0a96a997e53
enoskyeza <enoskyeza@gmail.com>
2024-01-08 23:28:59 +0300

    complete window tracker task

--- src/components/WindowTracker.js
+++ src/components/WindowTracker.js
@@ -2,15 +2,28 @@ import React from "react"
 import "./windowtracker.css"
 
 export default function WindowTracker() {
+    const [show, setShow] = React.useState(true)
+    const [windowSize, setWindowSize]  = React.useState(window.innerWidth)
+
+    React.useEffect(() => {
+        window.addEventListener('resize', function(){
+            setWindowSize(window.innerWidth)
+        })
+    }, [windowSize])
+
+    function handleToggle() {
+        setShow(prevState => !prevState)
+    }
+
     return (
         <>
-        <div className="window-container">
-            <button>
-                Toggle WindowTracker
-            </button>
+            <div className="window-container">
+                <button onClick={handleToggle}>
+                    Toggle WindowTracker
+                </button>
 
-            <h1>Window width: {window.innerWidth}</h1>
-        </div>
+                {show && <h1>Window width: {windowSize}</h1>}
+            </div>
         </>
     )
 }
